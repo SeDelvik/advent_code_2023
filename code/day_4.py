91b0de45dc88da52521c35575ebf0847c88e064a
adubrovina <adubrovina@at-consulting.ru>
2023-12-05 17:35:06 +0300

    complete day 4 part 2

--- code/day_4.py
+++ code/day_4.py
@@ -12,4 +12,25 @@ def part_1():
         sum = sum + cost
     return sum
 
+
+def part_2():
+    lines = open('../input/day_4.txt').read().split('\n')
+    cards = [1 for n in range(len(lines))]
+    for k in range(len(lines)):
+        count = 0
+        lists = lines[k].split(':')[1].split('|')
+        winners = [int(n.strip()) for n in lists[0].split(' ') if n != '']
+        nums = [int(n.strip()) for n in lists[1].split(' ') if n != '']
+        for i in nums:
+            if i in winners:
+                count = count + 1
+        for i in range(1, count + 1):
+            try:
+                cards[k + i] = cards[k + i] + cards[k]
+            except:
+                pass
+    return sum(cards)
+
+
 print(part_1())
+print(part_2())
